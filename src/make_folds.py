47451eb9c01af578f1bfe754eb6c963427ac466b
tatigabru <travellingtati@gmail.com>
2020-05-26 02:17:04 +0100

    Added folds and dataset

---
+++ src/make_folds.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from sklearn.model_selection import StratifiedKFold
+import numpy as np
+
+
+def make_folds(train_df: pd.DataFrame) -> pd.DataFrame:
+    bboxs = np.stack(train_df['bbox'].apply(lambda x: np.fromstring(x[1:-1], sep=',')))
+    for i, column in enumerate(['x', 'y', 'w', 'h']):
+        marking[column] = bboxs[:,i]
+    marking.drop(columns=['bbox'], inplace=True)
+    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+
+    df_folds = marking[['image_id']].copy()
+    df_folds.loc[:, 'bbox_count'] = 1
+    df_folds = df_folds.groupby('image_id').count()
+    df_folds.loc[:, 'source'] = marking[['image_id', 'source']].groupby('image_id').min()['source']
+    df_folds.loc[:, 'stratify_group'] = np.char.add(
+        df_folds['source'].values.astype(str),
+        df_folds['bbox_count'].apply(lambda x: f'_{x // 15}').values.astype(str)
+    )
+    df_folds.loc[:, 'fold'] = 0
+
+    for fold_number, (train_index, val_index) in enumerate(skf.split(X=df_folds.index, y=df_folds['stratify_group'])):
+        df_folds.loc[df_folds.iloc[val_index].index, 'fold'] = fold_number
+
+    return df_folds    
+
+
+if __name__=='__main__':
+
+    train_df = pd.read_csv('../input/global-wheat-detection/train.csv')
+    df_folds  = make_folds(train_df)
+    df_folds.to_csv() 
