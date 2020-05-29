40eb9b846aaf3cb8c8bc55bb277ba2a629b85555
tatigabru <travellingtati@gmail.com>
2020-05-29 13:02:44 +0100

    Added script to download data

---
+++ scripts/download_dataset.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+pip install --upgrade pip
+CUR_DIR=$pwd
+DATA_DIR_LOC=data
+
+cd ..
+mkdir -p $DATA_DIR_LOC
+cd $DATA_DIR_LOC
+
+if [ "$(ls -A $(pwd))" ]
+then
+    echo "$(pwd) not empty!"
+else
+    echo "$(pwd) is empty!"
+    pip install kaggle --upgrade
+    kaggle competitions download -c global-wheat-detection
+    unzip global-wheat-detection.zip
+    rm global-wheat-detection.zip  
+fi
+
+cd $CUR_DIR
+echo $(pwd)
